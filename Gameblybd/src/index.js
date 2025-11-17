ebee57c9ddff0561c4711641b419e2af8d0acaaf
OkarRojas <orojasb@unal.edu.co>
2025-11-16 19:14:28 -0500

    Ya esta funcionando bien

--- Gameblybd/src/index.js
+++ Gameblybd/src/index.js
@@ -13,6 +13,8 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3001',
+  'http://localhost:3002',
+  'http://localhost:3003',
   'https://OkarRojas.github.io'
 ];
 
